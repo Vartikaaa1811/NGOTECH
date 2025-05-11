6013e88cedea10071c1a310aa7044adb98015dc9
Vartika Sharma <vrtikaaa1811@gmail.com>
2025-05-11 23:18:21 +0530

    pushing frontend try

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -6,23 +6,23 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(express.json());
 
-app.get('/',(req, res) => {
-    res.send('back with express my love  for backend');
-})
+app.get("/", (req, res) => {
+  res.send("back with express my love  for backend");
+});
 
-app.get('/home',(req, res) => {
-    res.send('back with express my love  love love love for backend');
-})
-app.get('/homeii',(req, res) => {
-    res.send('back with express my love  love love love for backend');
-})
-app.get('/homee',(req, res) => {
-    res.send('back with express my love  love love love for backend');
-})
-app.get('/homeee',(req, res) => {
-    res.send('back with express my love  love love love for backend');
-})
+app.get("/home", (req, res) => {
+  res.send("back with express my love  love love love for backend");
+});
+app.get("/homeii", (req, res) => {
+  res.send("back with express my love  love love love for backend");
+});
+app.get("/homee", (req, res) => {
+  res.send("back with express my love  love love love for backend");
+});
+app.get("/homeee", (req, res) => {
+  res.send("back with express my love  love love love for backend");
+});
 
 app.listen(port, () => {
-    console.log(`Server is up and running on port ${port}`);
-})
+  console.log(`Server is up and running on port ${port}`);
+});
