c19752ec1151263e59b35ae8dba8baab54575854
Vartika Sharma <vrtikaaa1811@gmail.com>
2025-05-12 18:38:00 +0530

    Re-add frontend folder

---
+++ frontend/src/app/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const footer = () => {
+  return (
+    <>
+      <div>about us</div>
+      <div>contact</div>
+      <div>home</div>
+      <div>Login/signup</div>
+      <div>Terms and conditions</div>
+    </>
+  );
+};
+
+export default footer;
---
+++ frontend/src/app/page.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h2>hello back again</h2>
+      <h2>hello back again</h2>
+      <h2>hello back again</h2>
+      <h2>hello back again</h2>
+      <h2>hello back again</h2>
+      <h2>hello back again</h2>
+      <h2>hello back again</h2>
+      <h2>hello back again</h2>
+
+      <h2>hello back again</h2>
+      <h2>hello back again</h2>
+      <p>
+        chalo ji madam ek din hor barabd krdiya hopefully kl asal code kr dogiii
+      </p>
+    </div>
+  );
+};
+
+export default page;
