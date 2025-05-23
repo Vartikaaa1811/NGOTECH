e22b5f0f6a5f528ad571a3fc0922025f8cf7ad0d
Vartika Sharma <vrtikaaa1811@gmail.com>
2025-05-23 22:42:55 +0530

    auth backend

--- backend/controllers/userauth_controller.js
+++ backend/controllers/userauth_controller.js
@@ -1,5 +1,51 @@
 const User = require("../models/userauth");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
+exports.register = async (req, res) =>{
+    const {name, email, password} = req.body;
+    try{
+        let user = await User.findOne({email});
+        if(user){
+            return res.status(400).send("User already exists");
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        user = new User({ name, email, password: hashedPassword});
+        await user.save();
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        expiresIn : '1d',
+        res.status(201).json({token, user: {id: user._id, name:user.name, email: user.email } });
+        
+
+    }catch(err){
+        res.status(500).send(err.message);
+    }
+};
 
 
+exports.login = async(req, res) =>{
+    const {email, password} = req.body;
+
+    try{
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).send("User does not exist!! Signup first")
+        }
+        const ismatch = await bcrypt.compare(password, user.password);
+        if(!ismatch){ return res.status(400).send("Invalid credentials")}
+
+        const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET, {expiresIn: '1d'});
+
+        res.status(200).json({token, user: {id: user._id, name: user.name, email: user.email}});
+
+
+
+    }catch(err){
+        res.status(500).send(err.message || "Server error");
+    }
+
+}
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,30 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/mongo");
+// const router = require("./routes/authRoute");
+
+dotenv.config();
+connectDB();
+
+
 const app = express();
-
-const port = process.env.PORT || 5000;
-
+app.use(cors());
 //middleware
 app.use(express.json());
+
+
 
 app.get("/", (req, res) => {
   res.send("back with express my love  for backend");
 });
 
-app.get("/home", (req, res) => {
-  res.send("back with express my love  love love love for backend");
-});
+app.use("/api/auth", require("./routes/authroute"));
 
+
+
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
 });
